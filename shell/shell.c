a16f69a65c951426e5bb64226fba662f444d7e7e
hegek87 <hegelandka@vcu.edu>
2013-12-28 14:10:34 -0500

    Working on the shell

--- shell/shell.c
+++ shell/shell.c
@@ -11,7 +11,6 @@
 
 int main(void){
 	while(1){
-		char in[BUFFER_SIZE];
 		/*
 		pid_t result = fork();
 		int status;
@@ -22,10 +21,10 @@ int main(void){
 		}
 		*/
 		struct passwd *user_data = getpwuid(getuid());
+		char *command = NULL;
 		char host[BUFFER_SIZE];
 		char c_dir[BUFFER_SIZE];
-		int host_res = gethostname(host, BUFFER_SIZE);
-		if(host_res == -1){
+		if(gethostname(host, BUFFER_SIZE) == -1){
 			perror("Failed to get host name.");
 			exit(-1);
 		}
@@ -34,8 +33,51 @@ int main(void){
 			exit(-1);
 		}
 		printf("%s@%s %s %% ", user_data->pw_name, host, c_dir);
-		scanf("%s", in);
-		printf("%s\n", in);
+		size_t len = 0;
+		ssize_t in;
+		if((in = getline(&command, &len, stdin)) == -1){
+			perror("Line not read");
+			exit(-1);
+		}
+		//parse command
+		printf("%s\n", command);
+		char *parsed_cmd[BUFFER_SIZE];
+		char *token, *save_ptr;
+		int i;
+		char *delim = " \t";
+		for(i = 0, token = command; ; ++i, token = NULL){
+			parsed_cmd[i] = strtok_r(token, delim, &save_ptr);
+			if(!parsed_cmd[i]){
+				break;
+			}
+		}
+		i = 0;
+		while(parsed_cmd[i]){
+			*(parsed_cmd[i]+(strlen(parsed_cmd[i])-1))='\0';
+			++i;
+		}
+		
+		//If command is cd
+		if(!strcmp(parsed_cmd[0], "cd")){
+			printf("Comp success");
+			if(chdir(parsed_cmd[1])){
+				if(*(parsed_cmd[1]+2)=='\n'){
+					printf("YES");
+				}
+				else{
+					printf("NO");
+				}
+				printf("%s : No such file or directory exists\n", parsed_cmd[1]);
+			}
+		}
+		//command is exit
+		else if(strcmp(parsed_cmd[0], "exit")){
+			printf("Exiting\n");
+			exit(0);
+		}
+		//fork and execute command otherwise
+		//else{}
+		//printf("%s\n", in);
 	}
 	return 0;
 }
