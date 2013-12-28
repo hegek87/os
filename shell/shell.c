803e08acc38bf6fed9989fcae1eedfbb5d04e00b
hegek87 <hegelandka@vcu.edu>
2013-12-28 16:07:16 -0500

    Terminal makes system calls for cd and exit. It uses fork/exec to process all other commands

--- shell/shell.c
+++ shell/shell.c
@@ -9,30 +9,14 @@
 
 #define BUFFER_SIZE 4096
 
+int process_cmd(char **);
+void print_prompt(void);
+void parse_command(char *[], char *);
+
 int main(void){
 	while(1){
-		/*
-		pid_t result = fork();
-		int status;
-		//pid_t parent = getpid();
-		char *pwd_arg[2] = {"pwd", NULL};
-		if(result == 0){
-			execvp("pwd", pwd_arg);
-		}
-		*/
-		struct passwd *user_data = getpwuid(getuid());
+		print_prompt();
 		char *command = NULL;
-		char host[BUFFER_SIZE];
-		char c_dir[BUFFER_SIZE];
-		if(gethostname(host, BUFFER_SIZE) == -1){
-			perror("Failed to get host name.");
-			exit(-1);
-		}
-		if(!getcwd(c_dir, BUFFER_SIZE)){
-			perror("Failed to get working directory.");
-			exit(-1);
-		}
-		printf("%s@%s %s %% ", user_data->pw_name, host, c_dir);
 		size_t len = 0;
 		ssize_t in;
 		if((in = getline(&command, &len, stdin)) == -1){
@@ -40,44 +24,73 @@ int main(void){
 			exit(-1);
 		}
 		//parse command
-		printf("%s\n", command);
 		char *parsed_cmd[BUFFER_SIZE];
-		char *token, *save_ptr;
-		int i;
-		char *delim = " \t";
-		for(i = 0, token = command; ; ++i, token = NULL){
-			parsed_cmd[i] = strtok_r(token, delim, &save_ptr);
-			if(!parsed_cmd[i]){
-				break;
-			}
-		}
-		i = 0;
-		while(parsed_cmd[i]){
-			*(parsed_cmd[i]+(strlen(parsed_cmd[i])-1))='\0';
-			++i;
-		}
+		parse_command(parsed_cmd, command);
+		int i = 0;
 		
 		//If command is cd
 		if(!strcmp(parsed_cmd[0], "cd")){
-			printf("Comp success");
+			//remove '\n' from path
+			*(parsed_cmd[1]+(strlen(parsed_cmd[1])-1)) = '\0';
 			if(chdir(parsed_cmd[1])){
-				if(*(parsed_cmd[1]+2)=='\n'){
-					printf("YES");
-				}
-				else{
-					printf("NO");
-				}
-				printf("%s : No such file or directory exists\n", parsed_cmd[1]);
+				printf("%s : No such file or directory exists\n"
+					, parsed_cmd[1]);
 			}
 		}
 		//command is exit
-		else if(strcmp(parsed_cmd[0], "exit")){
-			printf("Exiting\n");
+		else if(!strcmp(parsed_cmd[0], "exit\n")){
 			exit(0);
 		}
 		//fork and execute command otherwise
-		//else{}
-		//printf("%s\n", in);
+		else{
+			//find last element and strip off '\n'
+			while(parsed_cmd[i])
+				++i;
+			int last_el = i-1;
+			*(parsed_cmd[last_el]+(strlen(parsed_cmd[last_el])-1)) = '\0';
+			process_cmd(parsed_cmd);
+		}
 	}
 	return 0;
 }
+
+int process_cmd(char **parsed_cmd){
+	pid_t child = fork();
+	int status;
+	if(!child){
+		execvp(parsed_cmd[0], parsed_cmd);
+	}
+	else{
+		wait(&status);
+	}
+	return status;
+}
+
+void print_prompt(void){
+	struct passwd *user_data = getpwuid(getuid());
+	char host[BUFFER_SIZE];
+	char c_dir[BUFFER_SIZE];
+	if(gethostname(host, BUFFER_SIZE) == -1){
+		perror("Failed to get host name.");
+		exit(-1);
+	}
+	if(!getcwd(c_dir, BUFFER_SIZE)){
+		perror("Failed to get working directory.");
+		exit(-1);
+	}
+	printf("%s@%s %s %% ", user_data->pw_name, host, c_dir);
+}	
+
+void parse_command(char *parsed_cmd[], char *command){
+	char *token, *save_ptr;
+	int i;
+	char *delim = " \t";
+	for(i = 0, token = command; ; ++i, token = NULL){
+		parsed_cmd[i] = strtok_r(token, delim, &save_ptr);
+		if(!parsed_cmd[i]){
+			break;
+		}
+	}
+}
+
+
