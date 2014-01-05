82ab8b3008c887442c2a0712d8d2b472068f93b3
hegek87 <hegelandka@vcu.edu>
2014-01-04 22:04:03 -0500

    Buggy still

--- file_comp/file_comp.c
+++ file_comp/file_comp.c
@@ -43,7 +43,7 @@ int process(char *path){
 				printf("Path too long\n");
 				break;
 			case ENOENT:
-				printf("A component of the path doesn't exits\n");
+				printf("A component of the path doesn't exist\n");
 				break;
 			case ENOMEM:
 				printf("Out of kernel mem\n");
@@ -85,13 +85,14 @@ int process(char *path){
 				return 1;
 			}
 			if(strcmp(path, ".")){
-				int p_len = strlen(path) + strlen(dirp->d_name)+1;
-				char new_path[p_len+1];
+				int p_len = strlen(path) + strlen(dirp->d_name)+2;
+				char new_path[p_len];
 				strcpy(new_path, path);
 				strcat(new_path, "/");
 				strcat(new_path, dirp->d_name);
 				printf("Pushing: %s\n", new_path);
 				push_key(to_visit, new_path);
+				printf("Size of stack: %d\n", stack_size(to_visit));
 			}
 			else{
 				printf("Pushing: %s\n", dirp->d_name);
@@ -104,6 +105,7 @@ int process(char *path){
 	else{
 		printf("Found File\n");
 		printf("%s\n", path);
+		return 1;
 	}
 	return 0;
 }
@@ -124,19 +126,85 @@ int main(int argc, char **argv){
 	}
 	//start search at current working directory
 	else{
-		*path = '.';
+		//path = calloc(2, sizeof(char));
+		path = ".";
 	}
 	
 	push_key(to_visit, path);
-	
+	//char *cur;
 	while(!is_stack_empty(to_visit)){
-		char *cur = (char *)(pop(to_visit)->data);
-		//printf("Popped: %s\n", cur);
-		if(process(cur)==-1){
+		struct stat cur_obj;
+		path = (char *)(pop(to_visit)->data);
+		printf("%s\n", path);
+		if(lstat(path, &cur_obj)){
+			perror("Failed to stat\n");
+			printf("%d\n", errno);
 			exit(-1);
+		}
+		mode_t x = cur_obj.st_mode;
+		if(S_ISDIR(x)){
+			//found a directory
+			printf("Is a Directory\n");
+			DIR *cwd;
+			struct dirent *dirp;
+			if(!(cwd = opendir(path))){
+				perror("FAILED TO OPEN\n");
+				exit(-1);
+			}
+			while((dirp = readdir(cwd))){
+				printf("Name: %s\n", dirp->d_name);
+				if(!strcmp(dirp->d_name, ".")){
+					continue;
+				}
+				if(!strcmp(dirp->d_name, "..")){
+					continue;
+				}
+				if(strcmp(path, ".")){
+					//build path
+					int p_len = strlen(path)+strlen(dirp->d_name)+2;
+					char *new_path[p_len+1];
+					strcpy(new_path, path);
+					strcat(new_path, "/");
+					strcat(new_path, dirp->d_name);
+					push_key(to_visit, new_path);
+					printf("Pushed: %s\n", new_path);
+				}
+				else{
+					push_key(to_visit, dirp->d_name);
+				}
+			}
+		}
+		else{
+			printf("Not a Directory\n");
 		}
 	}
 	
+	/*
+	while(!is_stack_empty(to_visit)){
+		printf("Second loop\n");
+		printf("strlen of cur: %d\n", strlen(cur));
+		cur = (char *)(pop(to_visit)->data);
+		if(process(cur) == -1){
+			break;
+		}
+	}*/
+	/*
+	struct node *t = pop(to_visit);
+	printf("%s\n", t->data);
+	*/
+	/*
+	char *x = (char *)(pop(to_visit)->data);
+	printf("%s\n", x);
+	printf("strlen of x: %d\n", strlen(x));
+	x = (char *)(pop(to_visit)->data);
+	//printf("%s\n", x);
+	printf("strlen of x: %d\n", strlen(x));
+	
+	struct stat ab;
+	if(lstat(x, &ab)){
+		perror("FAILED\n");
+	}
+	*/
 	printf("Done processing. Stack size: %d\n", stack_size(to_visit));
 		
 	is_empty(visited);
