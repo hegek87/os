c2787f95a4359e35d872608a8e99b3902635493e
hegek87 <hegelandka@vcu.edu>
2014-01-10 17:45:42 -0500

    Working on my ls

--- my_ls/my_ls.c
+++ my_ls/my_ls.c
@@ -2,28 +2,49 @@
 #include <string.h>
 #include <dirent.h>
 #include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
 #include <strings.h>
+#include <stdlib.h>
 #include "my_ls.h"
 
 int process_path(char *path){
 	DIR *cd;
 	struct dirent *dirp;
+	struct stat c_obj;
 	
 	if(!(cd = opendir(path))){
 		perror("Failed to open directory");
 		return -1;
 	}
-	
-	int i = 0;
+	//printf("OPENED DIR\n");
 	while((dirp = readdir(cd))){
+		//printf("READING DIR\n");
 		if(!strcmp(dirp->d_name, ".") 
-			|| !strcmp(dirp->d_name, "..")){
+			|| !strcmp(dirp->d_name, "..")
+			|| dirp->d_name[0] == '.'){
 			continue;
 		}
-		printf("%s\t\t", dirp->d_name);
-		if(++i == 3){
-			printf("\n");
-			i = 0;
+		int p_len = strlen(dirp->d_name) + strlen(path)+2;
+		char *new_path = calloc(p_len, sizeof(char));
+		strcpy(new_path, path);
+		strcat(new_path, "/");
+		strcat(new_path, dirp->d_name);
+		if(lstat(new_path, &c_obj)){
+			printf("Failed to stat\n");
+			return -1;
+		}
+		mode_t x = c_obj.st_mode;
+		if(S_ISDIR(x)){
+			printf(BLUE "%-11s" RESET, dirp->d_name);
+		}
+		else if((x & S_IXUSR) != 0 
+				|| (x & S_IXOTH) != 0 
+				|| (x & S_IXGRP) != 0){
+			printf(GREEN "%-11s" RESET, dirp->d_name);
+		}
+		else{
+			printf("%-11s", dirp->d_name);
 		}
 	}
 	printf("\n");
@@ -40,8 +61,12 @@ int main(int argc, char **argv){
 	else{
 		path = ".";
 	}
+	//printf("PATH: %s\n", path);
 	//if(no flags){
-	process_path(path);
+	if(process_path(path)){
+		perror("Failed to process directory");
+		exit(-1);
+	}
 	//}
 	return 0;
 }
--- my_ls/my_ls.h
+++ my_ls/my_ls.h
@@ -1,3 +1,7 @@
 #include "../../Data-Structures/double_linked_list/double_linked_list.h"
 
+#define GREEN "\e[1;32m"
+#define BLUE "\e[1;34m"
+#define RESET "\e[0m"
+
 int process_path(char *);
